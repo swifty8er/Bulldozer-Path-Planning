38344e289d56bc162d7ba9cd4ce779ba4e5ee6cf
Elliott Smith <cbrakespear@hotmail.com.au>
2020-03-19 19:46:54 +1100

    Forward left curves

--- Python/Bulldozer Path Planning/Bulldozer Path Planning/RadiiTests.py
+++ Python/Bulldozer Path Planning/Bulldozer Path Planning/RadiiTests.py
@@ -1,12 +1,14 @@
 import turtle
+import time
+import math
 t = turtle.Pen()
-
-
+turtle.speed('fastest')
+turtle.tracer(False,5)
 t.up()
-t.back(600)
 t.left(90)
 t.back(300)
 t.right(90)
+t.back(300)
 t.down()
 for x in range(3):	
 	t.forward(600)
@@ -17,6 +19,7 @@ for x in range(3):
 	t.right(90)
 	t.forward(100)
 	t.right(90)
+
 
 t.forward(600)
 t.right(90)
@@ -39,5 +42,27 @@ t.up()
 t.forward(250)
 t.down()
 t.dot(2)
+turtle.tracer(True)
+#define the four minimum radius turns
 t.circle(100,90)
+t.circle(100,-180)
+t.circle(100,90)
+t.left(180)
+t.circle(100,90)
+t.circle(100,-180)
+t.circle(100,90)
+t.left(180)
+#define the forward and reverse left quadrant other turns
+p = 1.025
+inc = 0.025
+while (p<1.55):
+	radius = 100**p
+	length = (100*math.pi)/2.0
+	theta = (length/radius)*(180.0/math.pi) #convert radians to degrees
+	print(radius,theta,length)
+	t.circle(radius,theta)
+	t.circle(radius,-theta)
+	p+=inc
+	inc = inc*1.5
+
 turtle.done()
