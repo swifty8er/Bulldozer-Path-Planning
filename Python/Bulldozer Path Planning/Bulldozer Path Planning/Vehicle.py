420b5aeeeae8612848db085468ecc2c85811531b
Elliott Smith <cbrakespear@hotmail.com.au>
2020-03-22 20:22:51 +1100

    Scaffold vehicle class

---
+++ Python/Bulldozer Path Planning/Bulldozer Path Planning/Vehicle.py
@@ -0,0 +1,26 @@
+WIDTH = 1.0
+class Vehicle:
+    # initialise a vehicle, setting its x,y coord and heading
+    def __init__(self,x,y,theta):
+        self._x = x
+        self._y = y
+        self._theta = theta
+        self._boundaryLeftX = self._x - 0.5*WIDTH
+        self._boundaryRightX = self._x + 0.5*WIDTH
+        self._boundaryTopY = self._y + 0.5*LENGTH
+        self._boundaryBottomY = self._y - 0.5*LENGTH
+
+    @property
+    def getX(self):
+        return self._x
+    @property
+    def getY(self):
+        return self._y
+    @property
+    def getTheta(self):
+        return self._theta
+
+    def applyControl(self,radius,deltaTheta,direction):
+        pass #apply the control (radius,deltaTheta) in the direction specified to generate a new Vehicle object
+
+
