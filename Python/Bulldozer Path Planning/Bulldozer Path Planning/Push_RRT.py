49f9e62f33e6a5aac2508390e0af413e3d2e8038
Elliott Smith <cbrakespear@hotmail.com.au>
2020-04-21 12:48:38 +1000

    Found push points

--- Python/Bulldozer Path Planning/Bulldozer Path Planning/Push_RRT.py
+++ Python/Bulldozer Path Planning/Bulldozer Path Planning/Push_RRT.py
@@ -1,10 +1,23 @@
+import math
+from BasicGeometry import BasicGeometry
+from RRT import RRT
+from RRT import Status
+
 class Push_RRT:
     def __init__(self,map):
         self._map = map
         self._graph = {}
 
     def getPushPoints(self,disk_pos):
-        pass
+        push_points = []
+        angle = 0
+        while (angle<=2*math.pi):
+            push_point = (disk_pos[0]+self._map.disk_radius*math.cos(angle),disk_pos[1]+self._map.disk_radius*math.sin(angle))
+            heading = math.degrees(BasicGeometry.vec_angle(BasciGeometry.vec_from_points(disk_pos,push_points)))
+            push_pose = Vehicle(push_point[0],push_point[1],heading)
+            push_points.append(push_pose)
+            angle += (math.pi/6.0)
+        return push_points
 
     def addEdge(node1,node2,push_point,tree):
         pass
