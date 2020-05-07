cba0c1dad65b50f7c1786a18ceaf41ef82d8fe57
Elliott Smith <cbrakespear@hotmail.com.au>
2020-05-07 12:36:26 +1000

    Added PQState class

---
+++ Python/Bulldozer Path Planning/Bulldozer Path Planning/PQState.py
@@ -0,0 +1,13 @@
+class PQState:
+    def __init__(self,map,vehicle_pose,disk_positions,vehicle_path,disk_paths,rrt):
+        self._map = map
+        self._vehicle_pose = vehicle_pose
+        self._disk_positions = disk_positions
+        self._vehicle_path = vehicle_path
+        self_disk_paths = disk_paths
+        self_RRT = rrt
+
+
+
+
+
