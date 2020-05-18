845a19ceda690aa5278f462f4af32c5d8b03ded6
Elliott Smith <cbrakespear@hotmail.com.au>
2020-05-18 17:20:01 +1000

    Progress

--- Python/Bulldozer Path Planning/Bulldozer Path Planning/Octree.py
+++ Python/Bulldozer Path Planning/Bulldozer Path Planning/Octree.py
@@ -1,18 +1,32 @@
+import math
+from Vehicle import Vehicle
 class Octree:
-    def __init__(self,state,max_distance,max_size=8):
+    def __init__(self,state : Vehicle ,max_distance,max_size=8):
         self._centreState = state
         self._max_distance = max_distance
         self._max_size = max_size
         self._has_children = False
-        self._children = [] #list of Octrees
-        self._vehicle_states = [] #list of vehicles
+        self._children : Octree = [] #list of Octrees
+        self._vehicle_states : Vehicle = [] #list of vehicles
         self._num_states = 0
 
+    def extend(self):
+        self._has_children = True
+        self._children = list([self.generateChildTree(index) for index in range(self._max_size)])
 
-    def addState(self,state):
+
+    def getMaxDistanceBetweenVehicleStatesAndCentreState(self):
+
+
+    def generateChildTree(self,index):
+        parentState = self.__vehicle_states[index]
+        new_max_dist = self.getMaxDistanceBetweenVehicleStatesAndCentreState()
+        return Octree(parentState,new_max_dist)
+
+    def addState(self,state : Vehicle):
         if self._has_children:
             self.addStateToChild(state)
-        elif len(self._vehicle_states) > self._max_size:
+        elif len(self._vehicle_states) >= self._max_size:
             self.extend()
             for vs in self._vehicle_states:
                 self.addStateToChild(vs)
