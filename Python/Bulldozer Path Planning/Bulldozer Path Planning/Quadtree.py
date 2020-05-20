5ea30056d030fe1f3c5d651b32d530450a613137
Elliott Smith <cbrakespear@hotmail.com.au>
2020-05-20 21:15:48 +1000

    Fixed quadtree

--- Python/Bulldozer Path Planning/Bulldozer Path Planning/Quadtree.py
+++ Python/Bulldozer Path Planning/Bulldozer Path Planning/Quadtree.py
@@ -63,8 +63,22 @@ class Quadtree:
         return closestChildIndex
 
     def generateChildTree(self,index):
-        parentState = self._vehicle_states[index]
+        parentState = self.getParentState(index)
         return Quadtree(parentState,self,self._max_distance/2.0,self._max_size)
+
+
+    def getParentState(self,i):
+        half_dist = self._max_distance/2.0
+        if i == 0:
+            return Vehicle(self.centreState.x+half_dist,self.centreState.y+half_dist,0)
+        elif i == 1:
+            return Vehicle(self.centreState.x+half_dist,self.centreState.y-half_dist,0)
+        elif i == 2:
+            return Vehicle(self.centreState.x-half_dist,self.centreState.y+half_dist,0)
+        elif i == 3:
+            return Vehicle(self.centreState.x-half_dist,self.centreState.y-half_dist,0)
+        else:
+            raise Exception("Invalud index %d passed to get parent state" % (i))
 
     def addState(self,state : Vehicle):
         if self._has_children:
